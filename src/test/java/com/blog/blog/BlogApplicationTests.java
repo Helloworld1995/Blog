1512637004f147f17d423223eb610df46b8858fd
Helloworld1995 <46309761+helloworld1995@users.noreply.github.com>
2020-12-07 15:34:39 +0800

    2020/12/7

--- src/test/java/com/blog/blog/BlogApplicationTests.java
+++ src/test/java/com/blog/blog/BlogApplicationTests.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,21 +15,7 @@ public class BlogApplicationTests {
     RedisTemplate redisTemplate;
     @Test
     public void contextLoads() {
-        com.blog.vo.Test t=new com.blog.vo.Test();
-        t.setId(1);
-        t.setName("狗哥");
-        t.setAddress("深圳市南山区");
-        List<String> list=new ArrayList<>();
-        list.add("aaa");
-        list.add("bbb");
-        list.add("ccc");
-        redisTemplate.opsForValue().set("aaa",t);
-        Object o = redisTemplate.opsForValue().get("aaa");
-        System.out.println(o.toString());
 
     }
-    @Test
-    public void testRedis(){
-        System.out.println(redisTemplate.opsForHash().entries("comment"));
-    }
+
 }
